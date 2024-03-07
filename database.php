677b73511f00303f365bf69021f31093d9a72bf8
alhernandez25 <hernalej@oregonstate.edu>
2024-03-07 18:10:04 -0500

    enemy added in database file

--- database.php
+++ database.php
@@ -83,6 +83,90 @@ else if (isset($_POST["updatePlayer"])) {
 
     header("location: ./index.php");
 }
+else if (isset($_POST["addEnemy"])) {
+    // Retrieve form data
+    $name = $_POST["enemyName"];
+    $max_health = $_POST["maxHealth"];
+    $location_id = $_POST["enemyLocation"];
+    $ability1_id = $_POST["enemyAbility1"];
+    $ability2_id = $_POST["enemyAbility2"];
+
+    // Insert enemy
+    $sql = "INSERT INTO Enemies (name, health, max_health, location_id) VALUES (?, ?, ?, ?)";
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "siii", $name, $max_health, $max_health, $location_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    // Get the ID of the newly inserted enemy
+    $enemy_id = mysqli_insert_id($connection);
+
+    // Insert first M:M enemy:ability relationship
+    $sql = "INSERT INTO EnemyDetails (enemy_id, ability_id) VALUES (?, ?)";
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "ii", $enemy_id, $ability1_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    // Insert second M:M enemy:ability relationship
+    $sql = "INSERT INTO EnemyDetails (enemy_id, ability_id) VALUES (?, ?)";
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "ii", $enemy_id, $ability2_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    header("location: ./admin.php");
+}
+else if (isset($_POST["updateEnemy"])) {
+    $enemyID = $_POST["enemyID"];
+    $name = $_POST["enemyName"];
+    $location_id = $_POST["enemyLocation"];
+    $ability1_id = $_POST["enemyAbility1"];
+    $ability2_id = $_POST["enemyAbility2"];
+
+    // Update enemy name and location
+    $sql = "UPDATE Enemies SET name = ?, location_id = ? WHERE enemy_id = ?";
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "sii", $name, $location_id, $enemyID);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    // Update enemy abilities
+    $sql = "UPDATE EnemyDetails SET ability_id = ? WHERE enemy_details_id = ?";
+
+    // Update first M:M enemy:ability relationship
+    $stmt1 = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt1, $sql);
+    mysqli_stmt_bind_param($stmt1, "ii", $ability1_id, $_POST["enemyDetails1"]);
+    mysqli_stmt_execute($stmt1);
+    mysqli_stmt_close($stmt1);
+
+    // Update second M:M enemy:ability relationship
+    $stmt2 = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt2, $sql);
+    mysqli_stmt_bind_param($stmt2, "ii", $ability2_id, $_POST["enemyDetails2"]);
+    mysqli_stmt_execute($stmt2);
+    mysqli_stmt_close($stmt2);
+
+    header("location: ./admin.php");
+}
+else if (isset($_POST["deleteEnemy"])) {
+    // Delete enemy - CASCADES and deletes M:M enemy:abilities relationship
+    $sql = "DELETE FROM Enemies WHERE enemy_id = ?;";
+
+    $stmt = mysqli_stmt_init($connection);
+
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $_POST["enemy_id"]);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    header("location: ./admin.php");
+}
 else if (isset($_POST["addLocation"])) {
 
 }
